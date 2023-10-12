efa4a935d507978c668674d7829a1596bb74ec6a
gift56 <efeasiughu@gmail.com>
2023-10-12 17:45:50 -0400

    creating endpoints

--- server/src/server.js
+++ server/src/server.js
@@ -6,7 +6,9 @@ app.get("/hello", (req, res) => {
   res.send("Hello It's Efe");
 });
 
-app.get("/products", (req, res) => {});
+app.get("/products", (req, res) => {
+    
+});
 
 app.get("/products/:productId", (req, res) => {});
 
