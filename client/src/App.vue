a1b83c1a5515dc84ee96899be48f22e12468a00b
gift56 <efeasiughu@gmail.com>
2023-10-12 15:21:33 -0400

    working on styles

--- client/src/App.vue
+++ client/src/App.vue
@@ -4,14 +4,7 @@ import { RouterLink, RouterView } from "vue-router";
 
 <template>
   <header>
-    <div class="wrapper">
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/products">Products</RouterLink>
-        <RouterLink to="/products/1">Products Detail</RouterLink>
-        <RouterLink to="/productsdhbhd">Click</RouterLink>
-      </nav>
-    </div>
+   
   </header>
 
   <RouterView />
