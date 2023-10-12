de2360a3db24cab21fb4dea16996f246bc26423a
gift56 <efeasiughu@gmail.com>
2023-10-12 16:20:52 -0400

    working on product detail page

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import ProductsPage from "../views/ProductsPage.vue";
 import ProductDetailPage from "../views/ProductDetailPage.vue";
 import NotFoundPage from "../views/NotFoundPage.vue";
+import ShoppingCartPage from "../views/ShoppingCartPage.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -15,6 +16,10 @@ const router = createRouter({
       path: "/products/:productId",
       name: "productsdetails",
       component: ProductDetailPage,
+    },
+    {
+      path: "/cart",
+      component: ShoppingCartPage,
     },
     {
       path: "/:pathMatch(.*)*",
---
+++ client/src/views/ShoppingCartPage.vue
@@ -0,0 +1,9 @@
+<script setup>
+import { cartItems } from '@/constant';
+
+
+</script>
+
+<template>
+  <div>Shopping</div>
+</template>
