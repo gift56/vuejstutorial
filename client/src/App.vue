7abf3c116cd3eb56f9c49d01531ecf2591f1fcfc
gift56 <efeasiughu@gmail.com>
2023-10-12 13:26:29 -0400

    working on some stuff

--- client/src/App.vue
+++ client/src/App.vue
@@ -8,6 +8,8 @@ import { RouterLink, RouterView } from "vue-router";
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/products">Products</RouterLink>
+        <RouterLink to="/products/1">Products Detail</RouterLink>
+        <RouterLink to="/productsdhbhd">Click</RouterLink>
       </nav>
     </div>
   </header>
