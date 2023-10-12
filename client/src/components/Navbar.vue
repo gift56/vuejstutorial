b50e390e432229ea0294ab65af995234856f6255
gift56 <efeasiughu@gmail.com>
2023-10-12 15:50:18 -0400

    working on styles

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -1,5 +1,16 @@
-<script setup></script>
+<script setup>
+import { RouterLink } from "vue-router";
+</script>
 
 <template>
-  <div>Navbar</div>
+  <div class="nav-bar">
+    <RouterLink to="/products" class="products-link">
+      <div class="logo-wrap">
+        <img :src="logo" />
+      </div>
+    </RouterLink>
+    <RouterLink to="/cart" class="cart-link">
+      <button>Shopping Cart</button>
+    </RouterLink>
+  </div>
 </template>
