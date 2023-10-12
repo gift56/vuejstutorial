4c4419eb385ff1c29f19cf81bdd76df64ea54319
gift56 <efeasiughu@gmail.com>
2023-10-12 11:13:32 -0400

    working on some stuff

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -11,9 +11,9 @@ const router = createRouter({
       component: HomeView,
     },
     {
-      path: "/about",
-      name: "about",
-      component: AboutViewVue,
+      path: "/products",
+      name: "products",
+      component: ,
     },
   ],
 });
