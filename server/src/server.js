ba045fe7607dfb7594490364eb30d173c6fc8661
gift56 <efeasiughu@gmail.com>
2023-10-12 17:41:51 -0400

    creating endpoints

--- server/src/server.js
+++ server/src/server.js
@@ -6,9 +6,11 @@ app.get("/hello", (req, res) => {
   res.send("Hello It's Efe");
 });
 
-app.get("/products", (req, res) => {
-    
-  });
+app.get("/products", (req, res) => {});
+
+app.get("/products/:productId", (req, res) => {});
+
+app.get("/cart", (req, res) => {});
 
 app.listen(8000, () => {
   console.log("Server is listening at port 8000");
