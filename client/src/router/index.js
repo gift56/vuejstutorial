abddd5eb787d991870014b51e2f4754f984bad41
gift56 <efeasiughu@gmail.com>
2023-10-12 15:22:45 -0400

    working on styles

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -4,17 +4,11 @@ import ProductsPage from "../views/ProductsPage.vue";
 import ProductDetailPage from "../views/ProductDetailPage.vue";
 import NotFoundPage from "../views/NotFoundPage.vue";
 
-
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: "/",
-      name: "home",
-      component: HomeView,
-    },
-    {
-      path: "/products",
       name: "products",
       component: ProductsPage,
     },
