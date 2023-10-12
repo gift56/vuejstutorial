508a11b5baead1f472e08f66ec39ae44a52f55f9
gift56 <efeasiughu@gmail.com>
2023-10-12 17:20:20 -0400

    working on server.js file

--- server/src/server.js
+++ server/src/server.js
@@ -1,6 +1,11 @@
 import express from "express";
 
 const app = express();
+
+app.get("/hello", (req, res) => {
+  res.send("Hello It's Efe");
+});
+
 app.listen(8000, () => {
   console.log("Server is listening at port 8000");
 });
