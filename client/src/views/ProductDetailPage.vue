d2795727dfe4407548540819c6bee16d2bc381a7
gift56 <efeasiughu@gmail.com>
2023-10-12 16:08:59 -0400

    working on product detail page

--- client/src/views/ProductDetailPage.vue
+++ client/src/views/ProductDetailPage.vue
@@ -1,6 +1,14 @@
-<script setup>
+<script>
 import { products } from "@/constant";
 
+export default {
+  name: "ProductDetailPage",
+  data() {
+    return {
+      product: products.find((item) => item.id === $route.params.productId),
+    };
+  },
+};
 </script>
 
 <template>
