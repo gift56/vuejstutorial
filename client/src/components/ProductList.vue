b89926b3ea26f62794a763e4f75f33e771b08c8f
gift56 <efeasiughu@gmail.com>
2023-10-12 15:27:10 -0400

    working on styles

--- client/src/components/ProductList.vue
+++ client/src/components/ProductList.vue
@@ -1,3 +1,12 @@
 <template>
-  <div>ProductList</div>
+  <div class="grid-wrap">
+    <div class="product-item" v-for="product in products" :key="product.id">
+      <img :src="product.imageName" />
+      <h3 class="product-name">{{ product.name }}</h3>
+      <p class="product-price">{{ product.price }}</p>
+      <router-link :to="'/products/' + product.id">
+        <button>View Details</button>
+      </router-link>
+    </div>
+  </div>
 </template>
--- client/src/views/ProductsPage.vue
+++ client/src/views/ProductsPage.vue
@@ -1,5 +1,12 @@
+<script setup>
+import { products } from "@/constant";
+</script>
+
 <template>
   <header>
     <h1>Product</h1>
+    <div v-for="product in products">
+    
+    </div>
   </header>
 </template>
