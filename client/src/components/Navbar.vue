5604f035c0d4549d39d490f159efe2daeb97189f
gift56 <efeasiughu@gmail.com>
2023-10-12 15:57:16 -0400

    working on styles

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -3,14 +3,16 @@ import { RouterLink } from "vue-router";
 </script>
 
 <template>
-  <div class="nav-bar">
+  <div class="border border-[#ddd] h-[75px] flex items-center justify-between">
     <RouterLink to="/products" class="products-link">
-      <div class="logo-wrap">
-        <img :src="logo" />
+      <div class="w-[120px] aspect-square">
+        <img src="@/assets/logo-hexagon.svg" />
       </div>
     </RouterLink>
-    <RouterLink to="/cart" class="cart-link">
-      <button>Shopping Cart</button>
-    </RouterLink>
+    <div class="w-full flex items-center justify-end mr-4">
+      <RouterLink to="/cart" class="">
+        <button>Shopping Cart</button>
+      </RouterLink>
+    </div>
   </div>
 </template>
