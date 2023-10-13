2a44cc46fc5fb4ec92a775a080aaf926c069a394
gift56 <efeasiughu@gmail.com>
2023-10-13 08:41:36 -0400

    working on connecting db

--- server/src/server.js
+++ server/src/server.js
@@ -1,3 +1,20 @@
 import express from "express";
 
 const app = express();
+
+app.get("/hello", (req, res) => {
+  res.send("Eat a dick");
+});
+app.get("/products", (req, res) => {
+  res.send("Product");
+});
+app.get("/products/:productId", (req, res) => {
+  res.send("Product Detail");
+});
+app.get("/cart", (req, res) => {
+  res.send("cart");
+});
+
+app.listen(8000, () => {
+  console.log(`Server is running on http://localhost:${8000}`);
+});
