4fc6ff7781f6251cd2cf2aabe331ff2b0d30bf76
gift56 <efeasiughu@gmail.com>
2023-10-14 12:01:28 -0400

    integrating api

--- server/src/server.js
+++ server/src/server.js
@@ -13,7 +13,7 @@ async function start() {
   
   const app = express();
   app.use(express.json());
-  app.use("/images", express.static(path.join(__dirname, "../assets")));
+  app.use("/images/", express.static(path.join(__dirname, "../assets")));
 
   app.get("/api/products", async (req, res) => {
     const products = await db.collection("products").find({}).toArray();
