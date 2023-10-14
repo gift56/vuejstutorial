b6c94240a7eb48fddd06c4a83d5d918e95ae5b94
gift56 <efeasiughu@gmail.com>
2023-10-14 11:04:40 -0400

    integrating api

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,8 +6,10 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  devServer: {
-    proxy: "http://localhost:8000",
+  server: {
+    proxy: {
+      '/api':'http://localhost:8000',
+    },
   },
   resolve: {
     alias: {
