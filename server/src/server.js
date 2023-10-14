0cc11e5a9cb2d11ea8e772f782d2ef67402e95da
gift56 <efeasiughu@gmail.com>
2023-10-14 16:25:23 -0400

    working on deployment

--- server/src/server.js
+++ server/src/server.js
@@ -96,7 +96,7 @@ async function start() {
   const port = process.env.PORT || 8000;
 
   app.listen(port, () => {
-    console.log(`'Server is listening on port http://localhost:8000`);
+    console.log(`Server is listening on port http://localhost:${port}`);
   });
 }
 start();
