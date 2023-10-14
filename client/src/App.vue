9d44e7287d1c4f98f0b69688b09ccb968ab98d69
gift56 <efeasiughu@gmail.com>
2023-10-14 15:09:52 -0400

    working with firebase

--- client/src/App.vue
+++ client/src/App.vue
@@ -8,11 +8,23 @@
 <script>
 import { RouterView } from "vue-router";
 import Navbar from "@/components/Navbar.vue";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 export default {
   name: "App",
   components: {
     Navbar,
+  },
+  data() {
+    return {
+      user: null,
+    };
+  },
+  created() {
+    const auth = getAuth();
+    onAuthStateChanged(auth, (user) => {
+      this.user = user;
+    });
   },
 };
 </script>
