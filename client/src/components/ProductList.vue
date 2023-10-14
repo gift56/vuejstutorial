e72d0a1a1fa09df898e3259baabc6e571ca5a4f0
gift56 <efeasiughu@gmail.com>
2023-10-14 10:51:10 -0400

    integrating api

--- client/src/components/ProductList.vue
+++ client/src/components/ProductList.vue
@@ -1,10 +1,7 @@
-<script setup>
-import { RouterLink } from "vue-router";
-import { products } from "@/constant";
-</script>
-
 <template>
-  <div class="flex md:items-start items-center justify-center md:justify-start flex-wrap gap-4 mt-14">
+  <div
+    class="flex md:items-start items-center justify-center md:justify-start flex-wrap gap-4 mt-14"
+  >
     <div
       class="flex flex-col items-center gap-4 justify-center p-4 w-[90%] md:w-[32%] relative mb-3 rounded-lg shadow-cardShad"
       v-for="product in products"
@@ -22,3 +19,21 @@ import { products } from "@/constant";
     </div>
   </div>
 </template>
+
+<script>
+import { RouterLink } from "vue-router";
+import { products } from "@/constant";
+import axios from "axios";
+
+export default {
+  name: "ProductList",
+  data() {
+    return {
+      products,
+    };
+  },
+  async created() {
+    const res = await axios.get();
+  },
+};
+</script>
